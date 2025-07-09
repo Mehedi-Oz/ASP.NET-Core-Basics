a7365cf2f55642b29931e7d53fe08fe5ca59f44f
Muhammad Mehedi Hasan <mehedihasan199908@gmail.com>
2025-07-09 23:50:09 +0600

    TryParse

---
+++ TryParse/TryParse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string inputNumber = Console.ReadLine();
+
+            int num = 0;
+
+            bool success = int.TryParse(inputNumber, out num);
+
+            if (success)
+            {
+                Console.WriteLine($"{inputNumber} is converted to {num} successfully!!!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid number format. Failed to convert!");
+                Console.WriteLine($"{inputNumber} is not converted to int value successfully!!!");
+            }
+
+        }
+    }
+}
