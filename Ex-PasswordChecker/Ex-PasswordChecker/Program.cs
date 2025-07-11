65bdc84e3b297ba8d97f4dda6e8c47c7256e2ef4
Muhammad Mehedi Hasan <mehedihasan199908@gmail.com>
2025-07-12 02:54:43 +0600

    ex- Print string in reverse, Password checker

---
+++ Ex-PasswordChecker/Ex-PasswordChecker/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_PasswordChecker
+{
+    internal class Program
+    {
+        /*
+         * Ask user to enter password, and store
+         * Ask user to enter password again, and store
+         * Check if they are both contain something
+           * If they are, print "Passwords match"
+           * If they are not, print "Passwords do not match" 
+           * If they are empty, print "Please enter a password
+         */
+        static void Main(string[] args)
+        {
+            string password, checkPassword;
+
+            Console.Write("Enter a password: ");
+            password = Console.ReadLine();
+
+            Console.Write("Enter the password again: ");
+            checkPassword = Console.ReadLine();
+
+            if (!String.IsNullOrEmpty(password))
+            {
+                if (!String.IsNullOrEmpty(checkPassword))
+                {
+                    if (password.Length >= 6 && checkPassword.Length >= 6)
+                    {
+
+                        if (password.Equals(checkPassword))
+                        {
+                            Console.WriteLine("Passwords Matched!!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Passwords do not match!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter 6 or more characters!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a confirmation password");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a password");
+            }
+        }
+    }
+}
---
+++ Ex-StringInReverse/Ex-StringInReverse/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Ex_StringInReverse
+{
+
+    /*
+     * Ask user to input message
+     * Print in order
+     * Print in reverse
+    */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a message: ");
+            string message = Console.ReadLine();
+
+            // This is a message
+
+            Console.WriteLine($"User's given message in order: {message}");
+            Console.WriteLine("\nUser's given message in reverse:\n");
+
+            int i = 0;
+            for (i = message.Length - 1; i >= 0; i--)
+            {
+                Console.Write(message[i]);
+                System.Threading.Thread.Sleep(200);
+            }
+        }
+    }
+}
