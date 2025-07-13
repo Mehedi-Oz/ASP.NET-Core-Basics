e7352e843a2d6a5e567a5486208d378a5f58f921
Muhammad Mehedi Hasan <mehedihasan199908@gmail.com>
2025-07-13 23:16:11 +0600

    array

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Enter a number: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("\nNumbers Array: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+
+            Console.Write("\nNumbers Array: ");
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+        }
+    }
+}
