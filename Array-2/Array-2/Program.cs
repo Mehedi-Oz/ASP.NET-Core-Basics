b00ec8e4c6966bf39da080e264b9a6626232532f
Muhammad Mehedi Hasan <mehedihasan199908@gmail.com>
2025-07-17 23:42:32 +0600

    Array Sorting

--- Array-2/Array-2/Program.cs
+++ Array-2/Array-2/Program.cs
@@ -10,6 +10,38 @@ namespace Array_2
     {
         static void Main(string[] args)
         {
+            /*Array Sorting*/
+
+            int[] numbers = new int[]
+            {
+                4,5,23,6,7,234,9,2,0, 3
+            };
+
+            Console.Write("Unsorted Array: ");
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Array.Sort(numbers);
+
+            Console.Write("\nSorted Array: ");
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Array.Reverse(numbers);
+
+            Console.Write("\nReversed Array: ");
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
         }
     }
 }
