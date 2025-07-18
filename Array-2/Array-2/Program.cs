637958b71f8baecf63aaf0b613f7d8db0e395a3d
Muhammad Mehedi Hasan <mehedihasan199908@gmail.com>
2025-07-18 23:09:46 +0600

    Array reverse, clear, IndexOf

--- Array-2/Array-2/Program.cs
+++ Array-2/Array-2/Program.cs
@@ -33,15 +33,81 @@ namespace Array_2
                 Console.Write($"{num} ");
             }
 
-            Array.Reverse(numbers);
+
+            /*Array Reverse*/
+
+            //Array.Reverse(numbers);
+
+            //Console.Write("\nReversed Array: ");
+
+            //foreach (int num in numbers)
+            //{
+            //    Console.Write($"{num} ");
+            //}
+
+            //Another Way to Reverse Array
+
+            int[] sortedArray = new int[numbers.Length];
+
+            int x = 0;
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                sortedArray[x] = numbers[i];
+                x++;
+            }
 
             Console.Write("\nReversed Array: ");
 
-            foreach (int num in numbers)
+            foreach (var num in sortedArray)
             {
                 Console.Write($"{num} ");
             }
 
+
+            /*Array Clearing*/
+
+
+            Array.Clear(numbers, 2, 5);
+
+            //Alternative of Array.Clear
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = default;
+            }
+
+            Console.Write("\nCleared Array: ");
+
+            foreach (var num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+
+
+            /*Array IndexOf*/
+
+            Console.Write("\nSorted Array: ");
+            foreach (var num in sortedArray)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Console.Write("\nEnter a Number to Search: ");
+
+            int search = Convert.ToInt32(Console.ReadLine());
+
+            //int position = Array.IndexOf(sortedArray, search);
+
+            int position = Array.IndexOf(sortedArray, search, 0, 5);
+
+            if (position > -1)
+            {
+                Console.WriteLine($"{search} is found at position {position + 1}");
+            }
+            else
+            {
+                Console.WriteLine($"{search} is not present in the array!");
+            }
         }
     }
 }
