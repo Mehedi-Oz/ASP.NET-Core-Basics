d568cd1a835a0d756a228330ba1153318f47bf52
Muhammad Mehedi Hasan <mehedihasan199908@gmail.com>
2025-07-19 23:30:04 +0600

    Dictionary

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Dictionary*/
+
+            Dictionary<int, string> names = new Dictionary<int, string>()
+            {
+                { 1, "Muhammad"},
+                { 2, "Mehedi"},
+                { 3, "Hasan"}
+            };
+
+            //names.Add(1, "Muhammad");
+            //names.Add(2, "Mehedi");
+            //names.Add(3, "Hasan");
+
+            foreach (var name in names)
+            {
+                Console.Write($"{name} ");
+            }
+        }
+    }
+}
