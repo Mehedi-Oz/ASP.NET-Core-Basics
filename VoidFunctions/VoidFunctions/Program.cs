1e9f06228802224dea1f6bf92f62354629ffeed5
Muhammad Mehedi Hasan <mehedihasan199908@gmail.com>
2025-07-28 23:46:20 +0600

    void functions

---
+++ VoidFunctions/VoidFunctions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (int.TryParse("123", out int result))
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
