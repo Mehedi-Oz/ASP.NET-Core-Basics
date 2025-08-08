4b33cdfb5922cb8b9f91ee670b30ae55bc46a9e1
Muhammad Mehedi Hasan <mehedihasan199908@gmail.com>
2025-08-08 23:42:57 +0600

    Out parameters

---
+++ Out parameters/Out parameters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out_parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
